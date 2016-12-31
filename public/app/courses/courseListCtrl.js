53929ae5c369ce7d1c0ce82a9fc878d57e2e1928
stepforwardtech <jrstrayhorn@gmail.com>
2016-12-31 13:18:51 -0500

    finishing course list

--- public/app/courses/courseListCtrl.js
+++ public/app/courses/courseListCtrl.js
@@ -6,4 +6,11 @@ angular.module('app').controller('courseListCtrl', ['$scope', 'courseService', f
         .then(function (courses) {
             $scope.courses = courses;
         });
+
+    $scope.sortOptions = [
+        {value:"title",text:"Sort by Title"},
+        {value:"published",text: "Sort by Publish Date"}
+    ];
+
+    $scope.sortOrder = $scope.sortOptions[0].value;
 }]);
